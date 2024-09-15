83c722ddcb4c3d5df045352fa0f32a4161fb1c05
Sohelrana2815 <sohel152302@gmail.com>
2024-09-15 21:05:00 +0600

    set up footer and navbar

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Shared/Navbar/Navbar";
+import Footer from "../Shared/Footer/Footer";
 
 const MainLayout = () => {
-    return (
-        <div>
-            <Outlet/>
-        </div>
-    );
+  return (
+    <div className="">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default MainLayout;
