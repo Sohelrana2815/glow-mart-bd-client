ed4c821ad54ad13567a76f306775eacdbeaf6b7a
Sohelrana2815 <sohel152302@gmail.com>
2024-09-15 21:20:47 +0600

    layout responsive adjusment

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -4,11 +4,15 @@ import Footer from "../Shared/Footer/Footer";
 
 const MainLayout = () => {
   return (
-    <div className="">
-      <Navbar />
-      <Outlet />
+    <>
+      <div className="max-w-screen-xl mx-auto min-h-screen flex flex-col px-4 mt-10">
+        <Navbar />
+        <div className="flex-grow">
+          <Outlet />
+        </div>
+      </div>
       <Footer />
-    </div>
+    </>
   );
 };
 
