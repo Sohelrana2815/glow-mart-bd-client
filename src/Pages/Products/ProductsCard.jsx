897cd59f542dc406b7de95b9b03d1fd1ee5dd992
Sohelrana2815 <sohel152302@gmail.com>
2024-09-16 11:25:02 +0600

    json data make and load data

--- src/Pages/Products/ProductsCard.jsx
+++ src/Pages/Products/ProductsCard.jsx
@@ -1,16 +1,20 @@
 import PropTypes from "prop-types";
+import { FaEye } from "react-icons/fa";
 const ProductsCard = ({ product }) => {
   const { name, retailPrice, image } = product;
   return (
     <div className="card bg-base-100 w-96 shadow-xl">
       <figure className="px-10 pt-10">
-        <img src={image} alt={name} className="rounded-xl" />
+        <img src={image} alt="product img" className="rounded-xl" />
       </figure>
       <div className="card-body items-center text-center">
         <h2 className="card-title">{name}</h2>
         <p>{retailPrice}</p>
         <div className="card-actions">
-          <button className="btn btn-primary">Add To Cart</button>
+          <button className="btn bg-[#185519] text-white ">Add To Cart</button>
+          <button className="btn bg-[#E8B86D]  text-white">
+            <FaEye className="text-lg" />
+          </button>
         </div>
       </div>
     </div>
