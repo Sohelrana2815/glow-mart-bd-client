f9d6a427d42dfa26604c2915b13c05dd85979cc6
Sohelrana2815 <sohel152302@gmail.com>
2024-09-23 12:59:01 +0600

    Tanstack query

--- src/Pages/CategoryPage/CategoryPageCard.jsx
+++ src/Pages/CategoryPage/CategoryPageCard.jsx
@@ -1,8 +1,53 @@
 import { FaEye } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import useAuth from "../../Hooks/useAuth";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
+import Swal from "sweetalert2";
 
 const CategoryPageCard = ({ product }) => {
-  const { name, image, _id } = product;
+  const { name, image, _id, retailPrice } = product;
+  const axiosPublic = useAxiosPublic();
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  const handleAddToCart = () => {
+    if (user && user?.email) {
+      const cartItem = {
+        productId: _id,
+        userEmail: user.email,
+        productName: name,
+        productPrice: retailPrice,
+        ProductImg: image,
+      };
+
+      axiosPublic.post("/carts", cartItem).then((res) => {
+        console.log(res.data);
+        if (res.data.insertedId) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${name} Added successfully!`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+    } else {
+      Swal.fire({
+        title: "You are not logged in",
+        text: "Please Login to Add To Cart Products",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, Login!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/login");
+        }
+      });
+    }
+  };
   return (
     <div className="card card-compact bg-base-100 w-3/4 shadow-xl">
       <figure>
@@ -11,7 +56,9 @@ const CategoryPageCard = ({ product }) => {
       <div className="card-body text-center">
         <h2 className="card-title">{name}</h2>
         <div className="card-actions justify-center ">
-          <button className="btn btn-md btn-primary">Add to Cart</button>
+          <button className="btn btn-md btn-primary" onClick={handleAddToCart}>
+            Add to Cart
+          </button>
           <Link to={`/productInfo/${_id}`}>
             <button className="btn bg-[#E8B86D] text-white">
               <FaEye className="text-lg" />
