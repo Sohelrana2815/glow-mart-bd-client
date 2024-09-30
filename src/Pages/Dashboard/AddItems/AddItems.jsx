ffd19d08ef6952fc7141fcc69e98b763144ac97e
Sohelrana2815 <sohel152302@gmail.com>
2024-09-30 06:27:54 +0600

    sucseed to upload image to the image bb

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -1,20 +1,32 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import { FaUtensils } from "react-icons/fa6";
+import useAxiosPublic from "../../../Hooks/useAxiosPublic";
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const AddItems = () => {
-  const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
-  const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
-
   const {
     register,
     handleSubmit,
     formState: { errors },
-    // Upload image file in image bb then get the url
   } = useForm();
 
-  const onSubmit = (data) => {
+  const axiosPublic = useAxiosPublic();
+
+  const onSubmit = async (data) => {
+    // Upload image file in image bb then get the url
+    // const
     console.log(data);
+    const imageFile = { image: data.image[0] };
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    });
+    console.log(res.data);
   };
 
   return (
