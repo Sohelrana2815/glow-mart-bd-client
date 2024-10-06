e878bbd41a3d9add40cc15715257182828c7f60d
Sohelrana2815 <sohel152302@gmail.com>
2024-10-06 22:15:01 +0600

    Try to sent email after payment but using mailgun , but i am not able to do this , because emailgun it's not free

--- src/Pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/Pages/Dashboard/AdminHome/AdminHome.jsx
@@ -3,6 +3,20 @@ import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import { FaDollarSign, FaUsers } from "react-icons/fa";
 import { FaBasketShopping } from "react-icons/fa6";
 import { BiSolidPurchaseTag } from "react-icons/bi";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  PieChart,
+  Pie,
+  ResponsiveContainer,
+  Legend,
+} from "recharts";
+const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "purple"];
 const AdminHome = () => {
   const axiosSecure = useAxiosSecure();
   const { data: stats = {} } = useQuery({
@@ -19,45 +33,151 @@ const AdminHome = () => {
       const res = await axiosSecure.get("/order-stats");
       return res.data;
     },
-  }); 
+  });
+
+  // Custom Shape For The BarCart
+
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${
+      x + width / 2
+    },${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
+      x + width
+    }, ${y + height}
+    Z`;
+  };
+  const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
+
+  // Custom pie chart
+
+  const pieChartData = chartData.map((data) => {
+    return { name: data.category, value: data.revenue };
+  });
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
 
   return (
-    <div className="stats shadow">
-      <div className="stat">
-        <div className="stat-figure text-secondary">
-          <FaDollarSign className="text-2xl" />
+    <>
+      <div className="stats shadow">
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaDollarSign className="text-2xl" />
+          </div>
+          <div className="stat-title">Revenue</div>
+          <div className="stat-value">${stats.revenue}</div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
         </div>
-        <div className="stat-title">Revenue</div>
-        <div className="stat-value">${stats.revenue}</div>
-        <div className="stat-desc">Jan 1st - Feb 1st</div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaUsers className="text-2xl" />
+          </div>
+          <div className="stat-title">Users</div>
+          <div className="stat-value">{stats.users}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaBasketShopping className="text-2xl" />
+          </div>
+          <div className="stat-title">Products</div>
+          <div className="stat-value">{stats.products}</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <BiSolidPurchaseTag className="text-2xl" />
+          </div>
+          <div className="stat-title">Orders</div>
+          <div className="stat-value">{stats.orders}</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
       </div>
 
-      <div className="stat">
-        <div className="stat-figure text-secondary">
-          <FaUsers className="text-2xl" />
+      <div className="flex">
+        <div className="w-1/2">
+          <BarChart
+            width={500}
+            height={300}
+            data={chartData}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="category" />
+            <YAxis />
+            <Bar
+              dataKey="quantity"
+              fill="#8884d8"
+              shape={<TriangleBar />}
+              label={{ position: "top" }}
+            >
+              {chartData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={colors[index % 6]} />
+              ))}
+            </Bar>
+          </BarChart>
         </div>
-        <div className="stat-title">Users</div>
-        <div className="stat-value">{stats.users}</div>
-        <div className="stat-desc">↗︎ 400 (22%)</div>
-      </div>
-
-      <div className="stat">
-        <div className="stat-figure text-secondary">
-          <FaBasketShopping className="text-2xl" />
+        <div className="w-1/2">
+          <ResponsiveContainer width="100%" height="100%">
+            <PieChart width={400} height={400}>
+              <Pie
+                data={pieChartData}
+                cx="50%"
+                cy="50%"
+                labelLine={false}
+                label={renderCustomizedLabel}
+                outerRadius={80}
+                fill="#8884d8"
+                dataKey="value"
+              >
+                {pieChartData.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                  />
+                ))}
+              </Pie>
+              <Legend />
+            </PieChart>
+          </ResponsiveContainer>
         </div>
-        <div className="stat-title">Products</div>
-        <div className="stat-value">{stats.products}</div>
-        <div className="stat-desc">↘︎ 90 (14%)</div>
       </div>
-      <div className="stat">
-        <div className="stat-figure text-secondary">
-          <BiSolidPurchaseTag className="text-2xl" />
-        </div>
-        <div className="stat-title">Orders</div>
-        <div className="stat-value">{stats.orders}</div>
-        <div className="stat-desc">↘︎ 90 (14%)</div>
-      </div>
-    </div>
+    </>
   );
 };
 
