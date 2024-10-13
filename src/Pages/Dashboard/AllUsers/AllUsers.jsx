1fead8260639ac5ab124f0894827e3f755527c45
Sohelrana2815 <sohel152302@gmail.com>
2024-10-14 04:23:26 +0600

    responsive done in AllUsers component

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -66,59 +66,99 @@ const AllUsers = () => {
   };
 
   return (
-    <div>
-      <h2 className="text-4xl">Total Users : {users.length}</h2>
-      <div className="px-5 mt-10">
-        <div className="overflow-x-auto">
-          <table className="table">
-            {/* head */}
-            <thead>
-              <tr className="text-base">
-                <th>#</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Role</th>
-                <th>Delete</th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* row 1 */}
-              {users.map((user, index) => (
-                <tr key={user._id}>
-                  <th className="text-lg">{index + 1}</th>
-                  <th className="text-lg">
-                    <p>{user.name}</p>
-                  </th>
-                  <th className="text-lg">{user.email}</th>
-                  <th>
-                    {user.role === "admin" ? (
-                      <span className="text-green-500 font-medium text-base">
-                        Admin
-                      </span>
-                    ) : (
-                      <button
-                        onClick={() => handleMakeAdmin(user)}
-                        className="btn bg-[#d1a054] "
-                      >
-                        <FaUsers className="text-white text-lg" />
-                      </button>
-                    )}
-                  </th>
-                  <th>
-                    <button
-                      onClick={() => handleDeleteUser(user)}
-                      className="btn bg-red-600 "
-                    >
-                      <FaTrashCan className="text-white text-lg" />
-                    </button>
-                  </th>
+    <>
+      <div className="hidden md:block lg:block">
+        <h2 className="text-4xl">Total Users : {users.length}</h2>
+        <div className="px-5 mt-10">
+          <div className="overflow-x-auto">
+            <table className="table">
+              {/* head */}
+              <thead>
+                <tr className="text-base">
+                  <th>#</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Role</th>
+                  <th>Delete</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {/* row 1 */}
+                {users.map((user, index) => (
+                  <tr key={user._id}>
+                    <th className="text-lg">{index + 1}</th>
+                    <th className="text-lg">
+                      <p>{user.name}</p>
+                    </th>
+                    <th className="text-lg">{user.email}</th>
+                    <th>
+                      {user.role === "admin" ? (
+                        <span className="text-green-500 font-medium text-base">
+                          Admin
+                        </span>
+                      ) : (
+                        <button
+                          onClick={() => handleMakeAdmin(user)}
+                          className="btn bg-[#d1a054] "
+                        >
+                          <FaUsers className="text-white text-lg" />
+                        </button>
+                      )}
+                    </th>
+                    <th>
+                      <button
+                        onClick={() => handleDeleteUser(user)}
+                        className="btn bg-red-600 "
+                      >
+                        <FaTrashCan className="text-white text-lg" />
+                      </button>
+                    </th>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-    </div>
+      {/* for small devices */}
+      <div className="px-2">
+        <p className="my-4 font-medium text-lg">Total Users : {users.length}</p>
+        <div className="my-4">
+          {users.map((user) => (
+            <div
+              className="p-3 space-y-5 border-2 my-4 rounded-lg "
+              key={user._id}
+            >
+              <p>Username : {user.name}</p>
+              <p>Email : {user.email}</p>
+
+              <p className="flex items-center gap-2">
+                <p className="text-primary font-medium">Role : </p>
+                {user.role === "admin" ? (
+                  <span className="text-green-500 font-medium">Admin</span>
+                ) : (
+                  <button
+                    onClick={() => handleMakeAdmin(user)}
+                    className="btn btn-sm bg-[#d1a054] "
+                  >
+                    <FaUsers className="text-white" />
+                  </button>
+                )}
+              </p>
+              <p className="flex items-center gap-2">
+                <p className="font-medium">Delete User : </p>
+                <button
+                  onClick={() => handleDeleteUser(user)}
+                  className="btn btn-sm bg-red-600 "
+                >
+                  <FaTrashCan className="text-white" />
+                </button>
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
