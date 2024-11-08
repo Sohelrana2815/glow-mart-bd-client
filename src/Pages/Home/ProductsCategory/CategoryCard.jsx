3e8281b959e4210c7ef78933e21e8c8c1d8f8daa
Sohelrana2815 <sohel152302@gmail.com>
2024-11-08 10:03:26 +0600

    Navigate to the all products page after clicking the category card

--- src/Pages/Home/ProductsCategory/CategoryCard.jsx
+++ src/Pages/Home/ProductsCategory/CategoryCard.jsx
@@ -1,21 +1,24 @@
 import "aos/dist/aos.css";
 import AnimatedComponent from "../../../Components/AnimatedComponent/AnimatedComponent";
+import { Link } from "react-router-dom";
 const CategoryCard = ({ singleCategory }) => {
   const { name, category, image } = singleCategory;
 
   return (
     <>
-      <AnimatedComponent animation="zoom-in-up">
-        <div className="card md:w-80  lg:w-96 bg-base-100 shadow-xl dark:bg-gray-800">
-          <figure className="lg:h-96 md:h-80 px-10 pt-16">
-            <img src={image} alt={name} className="rounded-xl" />
-          </figure>
-          <div className="card-body items-center text-center">
-            <h2 className="card-title">{name}</h2>
-            <p>{category.toUpperCase()}</p>
+      <Link to="/products">
+        <AnimatedComponent animation="zoom-in-up">
+          <div className="card md:w-80  lg:w-96 bg-base-100 shadow-xl dark:bg-gray-800">
+            <figure className="lg:h-96 md:h-80 px-10 pt-16">
+              <img src={image} alt={name} className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">{name}</h2>
+              <p>{category.toUpperCase()}</p>
+            </div>
           </div>
-        </div>
-      </AnimatedComponent>
+        </AnimatedComponent>
+      </Link>
     </>
   );
 };
